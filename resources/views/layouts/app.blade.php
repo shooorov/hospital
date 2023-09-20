a421df632cc2331202e143bed9155db7607761e2
Arif Hasan <arif.saiket@gmail.com>
2023-09-20 13:18:20 +0600

    layout setup ready

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,67 +14,79 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite([
+		'resources/sass/app.scss', 
+		'resources/css/app.css', 
+		'resources/js/app.js'
+	])
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+		<!-- navbar starts -->
+		<nav class="navbar navbar-expand-lg navbar-light bg-info py-0">
+			<div class="container">
+			<a class="navbar-brand py-0" href="index.html">
+				<img src="images/logo.jpg" alt="logo">
+			</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+				<li class="nav-item">
+					<a class="nav-link active text-uppercase oswald-reg fs-16 text-white" aria-current="page" href="index.html">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link text-uppercase oswald-reg fs-16 text-white" href="about us.html">About Us</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link text-uppercase oswald-reg fs-16 text-white" href="#">Doctor's Schedule</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link text-uppercase oswald-reg fs-16 text-white" href="#">Medical Record</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link text-uppercase oswald-reg fs-16 text-white" href="logindetail.html">Login</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link text-uppercase oswald-reg fs-16 text-white" href="#">Appointment</a>
+				</li>
+				
+				</ul>
+				
+			</div>
+			</div>
+		</nav>
+		<!-- navbar ends -->
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
         <main class="py-4">
             @yield('content')
         </main>
+
+		<!-- footer starts -->
+
+		<section id="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 m-auto">
+						<div class="footer-bottom">
+							<p class="mb-0 open-reg fs-14 text-center">Copyright &copy; All rights reserved by <span>HSGP</span>.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- footer ends -->
+
     </div>
+	
+	<script src="js/jquery-2.2.4.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/custom.js"></script>
+
 </body>
 </html>
